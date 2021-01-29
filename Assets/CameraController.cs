e412c091e5cba28c943bfbd8428a7ba6c1339edf
ltjax <marius.elvert@googlemail.com>
2021-01-29 22:42:15 +0100

    Playing around with camera and room generation

---
+++ Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private static readonly float ROOM_Y = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var camera = GetComponent<Camera>();
+
+        float ratio = Mathf.Tan(Mathf.Deg2Rad * camera.fieldOfView * 0.5f);
+        float height = ROOM_Y * 0.5f / ratio;
+
+        var position = transform.position;
+        
+        transform.position = new Vector3(position.x, position.y, -height);
+    }
+}
---
+++ Assets/GenerateRoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRoom : MonoBehaviour
+{
+    public Transform room;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 roomSize = room.localScale;
+
+        var position = new Vector2(0, 0);
+        for (int i = 0; i < 3; ++i)
+        {
+            position += roomSize;
+            var instance = GameObject.Instantiate(room);
+            instance.position = position;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
