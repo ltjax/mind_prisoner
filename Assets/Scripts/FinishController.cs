0083c5424beeb1c5bf4182100f77d27b5ea3e865
ltjax <marius.elvert@googlemail.com>
2021-01-31 12:56:38 +0100

    Can go to victory screen and back

---
+++ Assets/Scripts/FinishController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishController : MonoBehaviour
+{
+    void OnGameFinished()
+    {
+        SceneManager.LoadScene("Victory");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnGameFinished();
+        }
+    }
+}
---
+++ Assets/VictoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryController : MonoBehaviour
+{
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer <= 2.0f)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
